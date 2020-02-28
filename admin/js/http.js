1550243951bb002a52dc6f8d18ba391fd0a86ad9
小成 <1095081545@qq.com>
2020-02-28 15:50:38 +0800

    http.js文件提交

--- admin/js/http.js
+++ admin/js/http.js
@@ -1,28 +1,48 @@
 /* 沙箱模式 */
-(function(w){
-    var baseURL = 'http://localhost:8080/api/v1'
-    var BigNew = {
-        baseURL:baseURL,//基地址
-        user_login:      baseURL + '/admin/user/login',//用户登录
-        user_info:       baseURL + '/admin/user/info',//用户信息
-        user_detail:     baseURL + '/admin/user/detail',//用户详情
-        user_edit:       baseURL + '/admin/user/edit',//用户编辑
-        category_list:   baseURL + '/admin/category/list',//文章类别查询
-        category_add:    baseURL + '/admin/category/add',//文章类别新增
-        category_search: baseURL + '/admin/category/search',//文章类别搜索
-        category_edit:   baseURL + '/admin/category/edit',//文章类别编辑
-        category_delete: baseURL + '/admin/category/delete',//文章类别删除
-        article_query:   baseURL + '/admin/article/query',//文章搜索
-        article_publish: baseURL + '/admin/article/publish',//文章发布
-        article_search:  baseURL + '/admin/article/search',//文章信息查询
-        article_edit:    baseURL + '/admin/article/edit',//文章编辑
-        article_delete:  baseURL + '/admin/article/delete',//文章删除
-        comment_list:    baseURL + '/admin/comment/search',//文章评论列表
-        comment_pass:    baseURL + '/admin/comment/pass',//文章评论通过
-        comment_reject:  baseURL + '/admin/comment/reject',//文章评论不通过
-        comment_delete:  baseURL + '/admin/comment/delete',//文章评论删除
-    };
+(function(w) {
+  // 加载NProgress插件
+  const lib = `<link rel="stylesheet" href="./libs/nprogress/nprogress.css">
+    <script src="./libs/nprogress/nprogress.js"></script>`;
+  $("head").append(lib);
 
-    //暴露接口
-    w.BigNew = BigNew;
+  // ajax设置
+  $.ajaxSetup({
+    //   请求头
+    headers: { Authorization: localStorage.getItem("token") },
+    // 请求开始前
+    beforeSend: function() {
+      if (window.NProgress) NProgress.start();
+    },
+    // 请求完成后
+    complete: function() {
+      if (window.NProgress) NProgress.done();
+    }
+  });
+
+  // 请求地址
+  var baseURL = "http://localhost:8080/api/v1";
+  var BigNew = {
+    baseURL: baseURL, //基地址
+    user_login: baseURL + "/admin/user/login", //用户登录
+    user_info: baseURL + "/admin/user/info", //用户信息
+    user_detail: baseURL + "/admin/user/detail", //用户详情
+    user_edit: baseURL + "/admin/user/edit", //用户编辑
+    category_list: baseURL + "/admin/category/list", //文章类别查询
+    category_add: baseURL + "/admin/category/add", //文章类别新增
+    category_search: baseURL + "/admin/category/search", //文章类别搜索
+    category_edit: baseURL + "/admin/category/edit", //文章类别编辑
+    category_delete: baseURL + "/admin/category/delete", //文章类别删除
+    article_query: baseURL + "/admin/article/query", //文章搜索
+    article_publish: baseURL + "/admin/article/publish", //文章发布
+    article_search: baseURL + "/admin/article/search", //文章信息查询
+    article_edit: baseURL + "/admin/article/edit", //文章编辑
+    article_delete: baseURL + "/admin/article/delete", //文章删除
+    comment_list: baseURL + "/admin/comment/search", //文章评论列表
+    comment_pass: baseURL + "/admin/comment/pass", //文章评论通过
+    comment_reject: baseURL + "/admin/comment/reject", //文章评论不通过
+    comment_delete: baseURL + "/admin/comment/delete" //文章评论删除
+  };
+
+  //暴露接口
+  w.BigNew = BigNew;
 })(window);
